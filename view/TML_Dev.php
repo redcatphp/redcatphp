ee160c9548e6c7383da66b8679e586b08b0e3e19
Jo <jo@wildsurikat.com>
2014-05-30 16:41:52 +0200

    TML Dev helper

---
+++ view/TML_Dev.php
@@ -0,0 +1,19 @@
+<?php namespace surikat\view;
+use surikat\control;
+use ReflectionClass;
+class TML_Dev extends TML{
+	protected $hiddenWrap = true;
+	function load(){
+		$refl = new ReflectionClass('control');
+		$c = $refl->getConstants();
+		if(!empty($this->attributes)){
+			foreach($this->attributes as $dev)
+				if(control::devHas($c["dev_$dev"]))
+					return;
+		}
+		elseif(control::$DEV)
+			return;
+		$this->childNodes = array();
+	}
+	
+}
