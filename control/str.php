07e4905efe5c71da7821f0f1a32e0f233a5dd5a6
Jo <jo@wildsurikat.com>
2014-06-07 00:06:07 +0200

    add tolower helper for utf8

--- control/str.php
+++ control/str.php
@@ -1,5 +1,22 @@
 <?php namespace surikat\control;
 class str{
+	static function tolower($str){ //the strtolower version to support most amount of languages including russian, french and so on - http://www.php.net/manual/fr/function.strtolower.php
+		static $to = array(
+			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+			"v", "w", "x", "y", "z", "à", "á", "â", "ã", "ä", "å", "æ", "ç", "è", "é", "ê", "ë", "ì", "í", "î", "ï",
+			"ð", "ñ", "ò", "ó", "ô", "õ", "ö", "ø", "ù", "ú", "û", "ü", "ý", "а", "б", "в", "г", "д", "е", "ё", "ж",
+			"з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы",
+			"ь", "э", "ю", "я"
+		);
+		static $from = array(
+			"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+			"V", "W", "X", "Y", "Z", "À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï",
+			"Ð", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "Ø", "Ù", "Ú", "Û", "Ü", "Ý", "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж",
+			"З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ъ",
+			"Ь", "Э", "Ю", "Я"
+		);
+		return str_replace($from, $to, strtolower($str));
+	} 
 	static function html_convert_entities($string) {
 		return preg_replace_callback('/&([a-zA-Z][a-zA-Z0-9]+);/S',array('self','convert_entity'), $string);
 	}
