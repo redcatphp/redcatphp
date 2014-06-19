a517485fdef678e6f338f0f5d71e566155ce60ff
Jo <jo@wildsurikat.com>
2014-06-19 11:17:14 +0200

    surmenage bugfix static to abstract

--- control/filter.php
+++ control/filter.php
@@ -1,10 +1,12 @@
 <?php namespace surikat\control; 
 use surikat\control\PasswordHash;
 //think to look http://www.php.net/manual/en/filter.filters.sanitize.php
-static class filter{
+abstract class filter{
 	const BASIC_TAGS = 'br,p,a,strong,b,i,em,img,blockquote,code,dd,dl,hr,h1,h2,h3,h4,h5,h6,label,ul,li,span,sub,sup';
 	const ALL_TAGS = '!--,!DOCTYPE,a,abbr,acronym,address,applet,area,article,aside,audio,b,base,basefont,bdi,bdo,big,blockquote,body,br,button,canvas,caption,center,cite,code,col,colgroup,command,datalist,dd,del,details,dfn,dialog,dir,div,dl,dt,em,embed,fieldset,figcaption,figure,font,footer,form,frame,frameset,head,header,h1>-<h6,hr,html,i,iframe,img,input,ins,kbd,keygen,label,legend,li,link,map,mark,menu,meta,meter,nav,noframes,noscript,object,ol,optgroup,option,output,p,param,pre,progress,q,rp,rt,ruby,s,samp,script,section,select,small,source,span,strike,strong,style,sub,summary,sup,table,tbody,td,textarea,tfoot,th,thead,time,title,tr,track,tt,u,ul,var,video,wbr';
-	static function trim($v){ return trim($v); }
+	static function trim($v){
+		return trim($v);
+	}
 	static function rmpunctuation($v){
 		return preg_replace("/(?![.=$'€%-])\p{P}/u", '', $v);
 	}
