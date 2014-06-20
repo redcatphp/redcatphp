505849fc17d0cb1dd0495a4e7215534a09c8c1a7
Jo <jo@wildsurikat.com>
2014-06-20 17:10:58 +0200

    prepare convention

--- model/SimpleFacadeBeanHelper.php
+++ model/SimpleFacadeBeanHelper.php
@@ -5,6 +5,8 @@ use surikat\model\RedBeanPHP\BeanHelper\SimpleFacadeBeanHelper as RedBeanPHP_Sim
 class SimpleFacadeBeanHelper extends RedBeanPHP_SimpleFacadeBeanHelper{
 	public function getModelForBean(OODBBean $bean){
 		$c = model::getModelClass($bean->getMeta('type'));
+		//foreach($c::getDefColumns('write'))
+			//R::bindFunc('write', '$table.$col', '$func');
 		$model = new $c();
 		$model->loadBean($bean);
 		return $model;
