43dfd7a875def9ac4bdb75bb55d7f3ee9499d851
Jo <jo@wildsurikat.com>
2014-06-24 11:40:05 +0200

    clean model available - RedBean testConnection in 4.1

--- model.php
+++ model.php
@@ -4,21 +4,8 @@ use surikat\model\R;
 use surikat\model\Compo;
 use surikat\model\RedBeanPHP\OODBBean;
 class model {
-	private static $AVAILABLE;
 	const FLAG_ACCENT_INSENSITIVE = 2;
 	const FLAG_CASE_INSENSITIVE = 4;
-	static function available($force=null){
-		if(self::$AVAILABLE===null||$force){
-			self::$AVAILABLE = true;
-			try{
-				R::getCell('SELECT database()');
-			}
-			catch(\PDOException $e){
-				self::$AVAILABLE = false;
-			}
-		}
-		return self::$AVAILABLE;
-	}
 	static function storeArray(){
 		return call_user_func_array(array('surikat\model\R',__FUNCTION__),func_get_args());
 	}
