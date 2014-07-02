9cecc8715f529e900e3388a14db52c2f7524b100
Jo <jo@wildsurikat.com>
2014-07-03 00:09:16 +0200

    enhance csv methods and options

--- control/CsvIterator.php
+++ control/CsvIterator.php
@@ -8,26 +8,49 @@ class CsvIterator implements Iterator{
     private $currentElement;
     private $rowCounter;
     private $delimiter;
-	function __construct($file,$delimiter=',',$enclosure='"',$escape="\\",$length=0){
-        try {
-            $this->filePointer = fopen($file, 'r');
-            $this->delimiter = $delimiter;
-            $this->enclosure = $enclosure;
-            $this->escape = $escape;
-            $this->row_size = $length;
-            
-        }
-        catch(Exception $e){
+    private $trim;
+    private $keys;
+    private $keysKeepI;
+	function __construct($file,$delimiter=',',$enclosure='"',$escape="\\",$length=0,$trim=true,$keys=null,$keysKeepI=false){
+		$this->filePointer = fopen($file, 'r');
+		$this->delimiter = $delimiter;
+		$this->enclosure = $enclosure;
+		$this->escape = $escape;
+		$this->row_size = $length;            
+		$this->trim = true; 
+        if(!$this->filePointer)
             throw new Exception('The file "'.$file.'" cannot be read.');
-        }
+        if($keys)
+			$this->setKeys($keys);
+		$this->keysKeepI = $keysKeepI;
     }
+    function setKeys($keys,$keysKeepI=false){
+		$this->keys = $keys;
+		$this->keysKeepI = $keysKeepI;
+	}
     function rewind() {
         $this->rowCounter = 0;
         rewind($this->filePointer);
     }
     function current() {
-        $this->currentElement = fgetcsv($this->filePointer, $this->row_size, $this->delimiter, $this->enclosure, $this->escape);
+        $this->currentElement =  fgetcsv($this->filePointer, $this->row_size, $this->delimiter, $this->enclosure, $this->escape);
         $this->rowCounter++;
+        if($this->trim)
+			foreach($this->currentElement as &$v)
+				$v = trim($v);
+        if($this->keys){
+			$c = count($this->currentElement)-1;
+			for($i=0;$i<=$c;$i++){
+				if(isset($this->keys[$i])){
+					if($this->keys[$i]===true)
+						continue;
+					if($this->keys[$i]!==false)
+						$this->currentElement[$this->keys[$i]] = $this->currentElement[$i];
+				}
+				if(!$this->keysKeepI)
+					unset($this->currentElement[$i]);
+			}
+		}
         return $this->currentElement;
     }
     function key() {
