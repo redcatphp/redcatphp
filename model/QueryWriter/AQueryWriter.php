e97d16865a073a27902a79468085a3f1cd3b3b29
Jo <jo@wildsurikat.com>
2014-07-27 11:58:08 +0200

    add Cache tables

--- model/QueryWriter/AQueryWriter.php
+++ model/QueryWriter/AQueryWriter.php
@@ -1,5 +1,6 @@
 <?php namespace surikat\model\QueryWriter;
 trait AQueryWriter{
+	private static $_allTables = null;
 	function __get($k){
 		if(property_exists($this,$k))
 			return $this->$k;
@@ -7,5 +8,15 @@ trait AQueryWriter{
 	function addSqlColumnType($k,$v){
 		$this->typeno_sqltype[$k] = $v;
 		$this->sqltype_typeno[trim(strtolower($v))] = $k;
+	}
+	function createTable($table){
+		if(isset(self::$_allTables)&&!in_array($table,self::$_allTables))
+			self::$_allTables[] = $table;
+		return parent::createTable($table);
+	}
+	function getTables(){
+		if(!isset(self::$_allTables))
+			self::$_allTables = parent::getTables();
+		return self::$_allTables;
 	}
 }
