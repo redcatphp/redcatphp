ce1c94fae2402ef0befa9ea59be6fe69b94962b4
surikat <jo@surikat.pro>
2014-10-28 00:04:13 +0100

    noParse for CDATA and COMMENT

--- view/CDATA.php
+++ view/CDATA.php
@@ -3,6 +3,7 @@ class CDATA extends CORE{
 	var $nodeName = 'CDATA';
 	protected $hiddenWrap = true;
 	private $contentText = '';
+	protected $noParseContent = true;
 	function __construct($parent,$nodeName,$text,$constructor){
 		$this->parent = $parent;
 		$text = self::phpImplode($text,$constructor);
--- view/COMMENT.php
+++ view/COMMENT.php
@@ -2,6 +2,7 @@
 class COMMENT extends CORE{
 	protected $hiddenWrap = true;
 	private $contentText = '';
+	protected $noParseContent = true;
 	function __construct($parent,$nodeName,$text,$constructor){
 		$this->parent = $parent;
 		$text = self::phpImplode($text,$constructor);
