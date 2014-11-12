27061160c83eb60b687067a001a5283e84bd5f6a
surikat <jo@surikat.pro>
2014-11-12 17:11:26 +0100

    attach config loader to project root directory

--- control/Config.php
+++ control/Config.php
@@ -1,9 +1,10 @@
 <?php namespace surikat\control; 
+use control;
 class Config {
 	private static $factory = [];
 	static function __callStatic($f,$args){
 		if(!isset(self::$factory[$f]))
-			self::$factory[$f] = is_file($file=str_replace('_',DIRECTORY_SEPARATOR,$f).'/config.php')?include($file):false;
+			self::$factory[$f] = is_file($file=control::$CWD.str_replace('_',DIRECTORY_SEPARATOR,$f).'/config.php')?include($file):false;		
 		$ref =& self::$factory[$f];
 		foreach($args as $k)
 			if(is_array($ref)&&isset($ref[$k]))
