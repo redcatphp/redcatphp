ea5f438017fe2182154a563c04ea9285a4c505c6
surikat <jo@surikat.pro>
2014-11-22 23:51:17 +0100

    cleaning

--- view/CssSelector/Filter/CssParserFilterPseudoLast.php
+++ view/CssSelector/Filter/CssParserFilterPseudoLast.php
@@ -3,7 +3,6 @@ namespace surikat\view\CssSelector\Filter;
 use surikat\view\CssSelector\Filter\CssParserFilterPseudo;
 class CssParserFilterPseudoLast extends CssParserFilterPseudo{
 	function match($node, $position, $items){
-		//var_dump(count(func_get_arg(2)));exit;
 		return $position == (count($items) - 1);
 	}
 }
