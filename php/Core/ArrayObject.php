b9e85ac4fbb3773f971d638aeee299100ef825a9
surikat <jo@surikat.pro>
2014-12-08 01:49:00 +0100

    add deep merge_recursive method

--- php/Core/ArrayObject.php
+++ php/Core/ArrayObject.php
@@ -33,6 +33,26 @@ class ArrayObject extends \ArrayObject implements \ArrayAccess{
 			foreach($a as $k=>$v)
 				$this->offsetSet($k,$v);
 	}
+	static function merge_recursive(){
+		$args = func_get_args();
+		array_unshift($args,$this);
+		call_user_func_array(['self','array_merge_recursive'],$args);
+	}
+	static function array_merge_recursive(){
+		$args = func_get_args();
+		$merged = array_shift($args);
+		foreach($args as $array2){
+			if(!is_array($array2))
+				continue;
+			foreach($array2 as $key => &$value){
+				if(($value instanceof ArrayObject||is_array($value))&&isset($merged [$key])&&($merged[$key] instanceof ArrayObject||is_array($merged[$key])))
+					$merged[$key] = self::array_merge_recursive($merged[$key],$value);
+				else
+					$merged[$key] = $value;
+			}
+		}
+		return $merged;
+	}
 	function extend(){
 		$c = clone $this;
 		foreach(func_get_args() as $a)
