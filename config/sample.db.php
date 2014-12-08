65cfb40c63ade1cc43ab71d8788d7d2b2b8a3564
surikat <jo@surikat.pro>
2014-12-08 01:53:52 +0100

    add some config samples

---
+++ config/sample.db.php
@@ -0,0 +1,11 @@
+<?php return [
+	'type'			=>'mysql',
+	'host'			=>'localhost',
+	'name'			=>'surikat_project',
+	'user'			=>'root',
+	'password'		=>'root',
+	'port'			=>'',
+	'prefix'		=>'',
+	'freeze'		=>false,
+	'case'			=>true,
+];
