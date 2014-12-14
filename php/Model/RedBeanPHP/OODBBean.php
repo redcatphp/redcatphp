9094f2dec9b451627cd8f4f246a9316bfd5e27da
surikat <jo@surikat.pro>
2014-12-14 02:11:31 +0100

    add asc option to allow object dependency in validation process

--- php/Model/RedBeanPHP/OODBBean.php
+++ php/Model/RedBeanPHP/OODBBean.php
@@ -87,7 +87,21 @@ class OODBBean implements\IteratorAggregate,\ArrayAccess,\Countable
 	function __construct(Database $db){
 		$this->database = $db;
 	}
-	function store(){
+	function store($asc=true){
+		if($asc){
+			foreach($this as $o){
+				if(is_object($o)){
+					$this->database->store($o);
+				}
+				elseif(is_array($o)){
+					foreach($o as $_o){
+						if(is_object($_o)){
+							$this->database->store($_o);
+						}
+					}
+				}
+			}
+		}
 		$this->database->store($this);
 	}
 	
