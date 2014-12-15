e41dcbfdf6f8937f69656a308cad53b843fe1a8f
surikat <jo@surikat.pro>
2014-12-15 14:44:03 +0100

    rename to Arrays, cleaner methods name and add unique strict filter

--- php/Core/Arrays.php
+++ php/Core/Arrays.php
@@ -1,6 +1,6 @@
 <?php namespace Surikat\Core;
-class ArrayTools{
-	static function array_merge_recursive(){
+class Arrays{
+	static function merge_recursive(){
 		$args = func_get_args();
 		$merged = array_shift($args);
 		foreach($args as $array2){
@@ -9,7 +9,7 @@ class ArrayTools{
 			}
 			foreach($array2 as $key => &$value){
 				if(is_array($value)&&isset($merged [$key])&&is_array($merged[$key])){
-					$merged[$key] = self::array_merge_recursive($merged[$key],$value);
+					$merged[$key] = self::merge_recursive($merged[$key],$value);
 				}
 				else{
 					$merged[$key] = $value;
@@ -18,11 +18,21 @@ class ArrayTools{
 		}
 		return $merged;
 	}
-	static function array_values_recursive($key,$arr){
+	static function values_recursive($key,$arr){
 		$val = [];
 		array_walk_recursive($arr, function($v, $k) use($key, &$val){
 			if($k == $key) array_push($val, $v);
 		});
 		return count($val) > 1 ? $val : array_pop($val);
+	}
+	static function unique($arr){
+		return array_filter($arr, function($obj){
+			static $idList = array();
+			if(in_array($obj,$idList,true)){
+				return false;
+			}
+			$idList[] = $obj;
+			return true;
+		});
 	}
 }
