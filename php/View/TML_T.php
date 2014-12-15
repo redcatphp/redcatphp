65f613a44f9bf90e73efbc8761d143fe60141cf4
surikat <jo@surikat.pro>
2014-12-15 14:47:38 +0100

    add TML_T for transparent transation

---
+++ php/View/TML_T.php
@@ -0,0 +1,8 @@
+<?php namespace Surikat\View; 
+class TML_T extends TML{
+	function load(){
+		$this->attr('tmp-wrap',1);
+		if(isset($this->metaAttribution[0]))
+			$this->loadVars($this->metaAttribution[0]);
+	}
+}
