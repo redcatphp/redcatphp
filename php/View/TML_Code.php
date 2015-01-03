c221eedd6194a849f12ed0a75cd1c76bffc2b0f1
surikat <jo@surikat.pro>
2015-01-03 18:44:11 +0100

    add some helper

--- php/View/TML_Code.php
+++ php/View/TML_Code.php
@@ -3,9 +3,14 @@ class TML_Code extends TML{
 	protected $noParseContent = true;
 	function load(){
 		$stripTab = false;
+		$stripCurrentTab = false;
 		if($this->stripTab){
 			$this->removeAttr('stripTab');
 			$stripTab = true;
+		}
+		if($this->stripCurrentTab){
+			$this->removeAttr('stripCurrentTab');
+			$stripCurrentTab = true;
 		}
 		$this->remapAttr('file');
 		if($this->file){
@@ -27,6 +32,12 @@ class TML_Code extends TML{
 		$text = ltrim($text,"\n");
 		if($stripTab)
 			$text = str_replace("\t",'',$text);
+		if($stripCurrentTab){
+			$x = explode("\n",$text);
+			foreach($x as &$tx)
+				$tx = substr($tx,$this->indentationIndex()+2);
+			$text = implode("\n",$x);
+		}
 		if($this->parent->nodeName!='pre'&&!$this->keepNl){
 			$text = str_replace("\t","    ",$text);
 			$text = str_replace(" ","&nbsp;",$text);
