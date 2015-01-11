9b2369f3c4a39f4fd916736d8e391e76da02204c
surikat <jo@surikat.pro>
2015-01-11 17:01:32 +0100

    decouple FS from URI

--- php/Route/ByTml.php
+++ php/Route/ByTml.php
@@ -3,11 +3,17 @@ use Route\Faceted;
 class ByTml extends Faceted{
 	protected $match;
 	protected $dir = 'tml';
+	protected $dirFS = 'tml';
 	protected $dirHook;
-	function __construct($dir=null){
-		if(isset($dir)){
+	protected $dirHookFS;
+	function __construct($dir=null,$dirFS=null){
+		if(isset($dir)||isset($dirFS)){
+			if(!isset($dirFS))
+				$dirFS = $dir;
 			$this->dir = $dir;
+			$this->dirFS = $dirFS;
 			$this->dirHook = trim($dir,'/');
+			$this->dirHookFS = trim($dirFS,'/');
 		}
 	}
 	function __invoke(&$uri){
@@ -16,12 +22,12 @@ class ByTml extends Faceted{
 		$params = parent::__invoke($uri);
 		if($this->dirHook)
 			$params[0] = substr($params[0],strlen($this->dirHook));
-		$file = $this->dir.'/'.$params[0].'.tml';
+		$file = $this->dirFS.'/'.ltrim($params[0],'/').'.tml';
 		if(	is_file(SURIKAT_PATH.$file)
 			||is_file(SURIKAT_SPATH.$file))
 			return $params;
 	}
 	function getDirHook(){
-		return $this->dirHook;
+		return $this->dirHookFS;
 	}
 }
