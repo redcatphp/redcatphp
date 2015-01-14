8f718e380f7f45685df16b5099a455924eb71cd6
surikat <jo@surikat.pro>
2015-01-14 02:14:29 +0100

    auto add extension in include

--- php/View/TML_Include.php
+++ php/View/TML_Include.php
@@ -7,6 +7,8 @@ class TML_Include extends CALL_SUB{
 		$this->remapAttr('file');
 		$file = $this->__get('file');
 		$this->__unset('file');
+		if(!pathinfo($file,PATHINFO_EXTENSION))
+			$file .= '.tml';
 		$this->parseFile($file,$this->attributes,'include');
 	}
 }
