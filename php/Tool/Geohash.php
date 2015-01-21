fba68e6c28ce809126d8311ccd9cb207f6abe3c2
surikat <jo@surikat.pro>
2015-01-21 17:18:19 +0100

    add precision factor

--- php/Tool/Geohash.php
+++ php/Tool/Geohash.php
@@ -1,238 +1,170 @@
 <?php namespace Surikat\Tool;
-/**
- * Geohash generation class
- * http://blog.dixo.net/downloads/
- *
- * This file copyright (C) 2008 Paul Dixon (paul@elphin.com)
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
-
-
-
-/**
-* Encode and decode geohashes
-*
-*/
-abstract class Geohash
+class GeoHash
 {
-	private static $coding="0123456789bcdefghjkmnpqrstuvwxyz";
-	private static $codingMap=array();
+    private static $table = "0123456789bcdefghjkmnpqrstuvwxyz";
+    private static $bits = [
+        0b10000, 0b01000, 0b00100, 0b00010, 0b00001
+    ];
 	
-	static function initialize()
-	{
-		//build map from encoding char to 0 padded bitfield
-		for($i=0; $i<32; $i++)
-		{
-			self::$codingMap[substr(self::$coding,$i,1)]=str_pad(decbin($i), 5, "0", STR_PAD_LEFT);
-		}
-		
+	static function precision($num){
+		return (strlen(substr($num,strpos($num,'.')+1)))-1;
 	}
 	
-	/**
-	* Decode a geohash and return an array with decimal lat,long in it
-	*/
-	static function decode($hash)
-	{
-		//decode hash into binary string
-		$binary="";
-		$hl=strlen($hash);
-		for($i=0; $i<$hl; $i++)
-		{
-			$binary.=self::$codingMap[substr($hash,$i,1)];
-		}
-		
-		//split the binary into lat and log binary strings
-		$bl=strlen($binary);
-		$blat="";
-		$blong="";
-		for ($i=0; $i<$bl; $i++)
-		{
-			if ($i%2)
-				$blat=$blat.substr($binary,$i,1);
-			else
-				$blong=$blong.substr($binary,$i,1);
+    public static function encode($lng, $lat, $prec = null)
+    {
+		if(!isset($prec))
+			$prec = min(self::precision($lat),self::precision($lng));
+		$prec = pow(10,-$prec);
 			
-		}
-		
-		//now concert to decimal
-		$lat=self::binDecode($blat,-90,90);
-		$long=self::binDecode($blong,-180,180);
-		
-		//figure out how precise the bit count makes this calculation
-		$latErr=self::calcError(strlen($blat),-90,90);
-		$longErr=self::calcError(strlen($blong),-180,180);
-				
-		//how many decimal places should we use? There's a little art to
-		//this to ensure I get the same roundings as geohash.org
-		$latPlaces=max(1, -round(log10($latErr))) - 1;
-		$longPlaces=max(1, -round(log10($longErr))) - 1;
-		
-		//round it
-		$lat=round($lat, $latPlaces);
-		$long=round($long, $longPlaces);
-		
-		return array($lat,$long);
-	}
+        $minlng = -180;
+        $maxlng = 180;
+        $minlat = -90;
+        $maxlat = 90;
 
-	
-	/**
-	* Encode a hash from given lat and long
-	*/
-	static function encode($lat,$long)
-	{
-		//how many bits does latitude need?	
-		$plat=self::precision($lat);
-		$latbits=1;
-		$err=45;
-		while($err>$plat)
-		{
-			$latbits++;
-			$err/=2;
-		}
-		
-		//how many bits does longitude need?
-		$plong=self::precision($long);
-		$longbits=1;
-		$err=90;
-		while($err>$plong)
-		{
-			$longbits++;
-			$err/=2;
-		}
-		
-		//bit counts need to be equal
-		$bits=max($latbits,$longbits);
-		
-		//as the hash create bits in groups of 5, lets not
-		//waste any bits - lets bulk it up to a multiple of 5
-		//and favour the longitude for any odd bits
-		$longbits=$bits;
-		$latbits=$bits;
-		$addlong=1;
-		while (($longbits+$latbits)%5 != 0)
-		{
-			$longbits+=$addlong;
-			$latbits+=!$addlong;
-			$addlong=!$addlong;
-		}
-		
-		
-		//encode each as binary string
-		$blat=self::binEncode($lat,-90,90, $latbits);
-		$blong=self::binEncode($long,-180,180,$longbits);
-		
-		//merge lat and long together
-		$binary="";
-		$uselong=1;
-		while (strlen($blat)+strlen($blong))
-		{
-			if ($uselong)
-			{
-				$binary=$binary.substr($blong,0,1);
-				$blong=substr($blong,1);
-			}
-			else
-			{
-				$binary=$binary.substr($blat,0,1);
-				$blat=substr($blat,1);
-			}
-			$uselong=!$uselong;
-		}
-		
-		//convert binary string to hash
-		$hash="";
-		for ($i=0; $i<strlen($binary); $i+=5)
-		{
-			$n=bindec(substr($binary,$i,5));
-			$hash=$hash.self::$coding[$n];
-		}
-		
-		
-		return $hash;
-	}
-	
-	/**
-	* What's the maximum error for $bits bits covering a range $min to $max
-	*/
-	private static function calcError($bits,$min,$max)
-	{
-		$err=($max-$min)/2;
-		while ($bits--)
-			$err/=2;
-		return $err;
-	}
-	
-	/*
-	* returns precision of number
-	* precision of 42 is 0.5
-	* precision of 42.4 is 0.05
-	* precision of 42.41 is 0.005 etc
-	*/
-	private static function precision($number)
-	{
-		$precision=0;
-		$pt=strpos($number,'.');
-		if ($pt!==false)
-		{
-			$precision=-(strlen($number)-$pt-1);
-		}
-		
-		return pow(10,$precision)/2;
-	}
-	
-	
-	/**
-	* create binary encoding of number as detailed in http://en.wikipedia.org/wiki/Geohash#Example
-	* removing the tail recursion is left an exercise for the reader
-	*/
-	private static function binEncode($number, $min, $max, $bitcount)
-	{
-		if ($bitcount==0)
-			return "";
-		
-		#echo "$bitcount: $min $max<br>";
-			
-		//this is our mid point - we will produce a bit to say
-		//whether $number is above or below this mid point
-		$mid=($min+$max)/2;
-		if ($number>$mid)
-			return "1".self::binEncode($number, $mid, $max,$bitcount-1);
-		else
-			return "0".self::binEncode($number, $min, $mid,$bitcount-1);
-	}
-	
+        $hash = [];
+        $error = 180;
+        $isEven = true;
+        $chr = 0b00000;
+        $b = 0;
 
-	/**
-	* decodes binary encoding of number as detailed in http://en.wikipedia.org/wiki/Geohash#Example
-	* removing the tail recursion is left an exercise for the reader
-	*/
-	private static function binDecode($binary, $min, $max)
-	{
-		$mid=($min+$max)/2;
-		
-		if (strlen($binary)==0)
-			return $mid;
-			
-		$bit=substr($binary,0,1);
-		$binary=substr($binary,1);
-		
-		if ($bit==1)
-			return self::binDecode($binary, $mid, $max);
-		else
-			return self::binDecode($binary, $min, $mid);
-	}
+        while ($error >= $prec) {
+            if ($isEven) {
+                $next = ($minlng + $maxlng) / 2;
+                if ($lng > $next) {
+                    $chr |= self::$bits[$b];
+                    $minlng = $next;
+                } else {
+                    $maxlng = $next;
+                }
+            } else {
+                $next = ($minlat + $maxlat) / 2;
+                if ($lat > $next) {
+                    $chr |= self::$bits[$b];
+                    $minlat = $next;
+                } else {
+                    $maxlat = $next;
+                }
+            }
+            $isEven = !$isEven;
+
+            if ($b < 4) {
+                $b++;
+            } else {
+                $hash[] = self::$table[$chr];
+                $error = max($maxlng - $minlng, $maxlat - $minlat);
+                $b = 0;
+                $chr = 0b00000;
+            }
+        }
+
+        return join('', $hash);
+    }
+
+    public static function expand($hash)
+    {
+        list($minlng, $maxlng, $minlat, $maxlat) = self::decode($hash);
+        $dlng = ($maxlng - $minlng) / 2;
+        $dlat = ($maxlat - $minlat) / 2;
+
+        return [
+            self::encode($minlng - $dlng, $maxlat + $dlat),
+            self::encode($minlng + $dlng, $maxlat + $dlat),
+            self::encode($maxlng + $dlng, $maxlat + $dlat),
+            self::encode($minlng - $dlng, $maxlat - $dlat),
+            self::encode($maxlng + $dlng, $maxlat - $dlat),
+            self::encode($minlng - $dlng, $minlat - $dlat),
+            self::encode($minlng + $dlng, $minlat - $dlat),
+            self::encode($maxlng + $dlng, $minlat - $dlat),
+        ];
+    }
+
+    public static function getRect($hash)
+    {
+        list($minlng, $maxlng, $minlat, $maxlat) = self::decode($hash);
+
+        return [
+            [$minlng, $minlat],
+            [$minlng, $maxlat],
+            [$maxlng, $maxlat],
+            [$maxlng, $minlat],
+        ];
+    }
+
+    /**
+     * decode a geohash string to a geographical area
+     *
+     * @var $hash string geohash
+     * @return array [$minlng, $maxlng, $minlat, $maxlat];
+     */
+    public static function decode($hash,$prec=null)
+    {
+        $minlng = -180;
+        $maxlng = 180;
+        $minlat = -90;
+        $maxlat = 90;
+
+        for ($i=0,$c=strlen($hash); $i<$c; $i++) {
+            $v = strpos(self::$table, $hash[$i]);
+            if (1&$i) {
+                if (16&$v) {
+                    $minlat = ($minlat + $maxlat) / 2;
+                } else {
+                    $maxlat = ($minlat + $maxlat) / 2;
+                }
+                if (8&$v) {
+                    $minlng = ($minlng + $maxlng) / 2;
+                } else {
+                    $maxlng = ($minlng + $maxlng) / 2;
+                }
+                if (4&$v) {
+                    $minlat = ($minlat + $maxlat) / 2;
+                } else {
+                    $maxlat = ($minlat + $maxlat) / 2;
+                }
+                if (2&$v) {
+                    $minlng = ($minlng + $maxlng) / 2;
+                } else {
+                    $maxlng = ($minlng + $maxlng) / 2;
+                }
+                if (1&$v) {
+                    $minlat = ($minlat + $maxlat) / 2;
+                } else {
+                    $maxlat = ($minlat + $maxlat) / 2;
+                }
+            } else {
+                if (16&$v) {
+                    $minlng = ($minlng + $maxlng) / 2;
+                } else {
+                    $maxlng = ($minlng + $maxlng) / 2;
+                }
+                if (8&$v) {
+                    $minlat = ($minlat + $maxlat) / 2;
+                } else {
+                    $maxlat = ($minlat + $maxlat) / 2;
+                }
+                if (4&$v) {
+                    $minlng = ($minlng + $maxlng) / 2;
+                } else {
+                    $maxlng = ($minlng + $maxlng) / 2;
+                }
+                if (2&$v) {
+                    $minlat = ($minlat + $maxlat) / 2;
+                } else {
+                    $maxlat = ($minlat + $maxlat) / 2;
+                }
+                if (1&$v) {
+                    $minlng = ($minlng + $maxlng) / 2;
+                } else {
+                    $maxlng = ($minlng + $maxlng) / 2;
+                }
+            }
+        }
+        if($prec){
+			$minlng = round($minlng,$prec);
+			$maxlng = round($maxlng,$prec);
+			$minlat = round($minlat,$prec);
+			$maxlat = round($maxlat,$prec);
+		}
+        return [$minlng, $maxlng, $minlat, $maxlat];
+    }
 }
-Geohash::initialize();
