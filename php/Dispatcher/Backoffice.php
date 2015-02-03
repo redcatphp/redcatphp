0920e7337f01c181a4fa73ed83ebda4c753a0a0a
surikat <jo@surikat.pro>
2015-02-03 03:35:55 +0100

    lock the backoffice

--- php/Dispatcher/Backoffice.php
+++ php/Dispatcher/Backoffice.php
@@ -1,4 +1,5 @@
 <?php namespace Surikat\Dispatcher;
+use Surikat\Tool\Auth;
 class Backoffice extends ViewController{
 	protected $pathFS = 'backoffice';
 	function __construct(){
@@ -8,8 +9,12 @@ class Backoffice extends ViewController{
 		$this
 			->append(['new','Surikat\Route\Extension','css|js|png|jpg|jpeg|gif'],
 						['new','Surikat\Dispatcher\Synaptic',$this->pathFS])
-			->append(['new','Surikat\Route\ByTml','',$this->pathFS],$this)
+			->append(['new','Surikat\Route\ByTml','',$this->pathFS],function(){
+				Auth::lockServer(Auth::RIGHT_MANAGE);
+				return call_user_func_array($this,func_get_args());
+			})
 			->append(['new','Surikat\Route\ByPhp','',$this->pathFS],function($paths){
+				Auth::lockServer(Auth::RIGHT_MANAGE);
 				list($dir,$file,$adir,$afile) = $paths;
 				chdir($adir);
 				include $file;
