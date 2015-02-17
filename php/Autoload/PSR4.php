6ca13e579311c47c23d7219fbedfb42f3dc11a89
surikat <jo@surikat.pro>
2015-02-17 17:00:20 +0100

    improve design

--- php/Autoload/PSR4.php
+++ php/Autoload/PSR4.php
@@ -1,8 +1,37 @@
 <?php namespace Surikat\Autoload;
 use Exception;
-use Surikat\Autoload\Abstraction;
-require_once __DIR__.'/Abstraction.php';
-class PSR4 extends Abstraction{
+class PSR4{
+	protected $namespaces = [];
+	protected $checked = [];
+	function addNamespace($prefix, $base_dir, $prepend = false){
+		$prefix = trim($prefix, '\\').'\\';
+		$base_dir = rtrim($base_dir, '/').'/';
+		if(!isset($this->namespaces[$prefix]))
+			$this->namespaces[$prefix] = [];
+		if ($prepend)
+			array_unshift($this->namespaces[$prefix], $base_dir);
+		else
+			array_push($this->namespaces[$prefix], $base_dir);
+	}
+	protected function loadFile($file,$class){
+		if(file_exists($file)){
+			require $file;
+			if(!class_exists($class,false)&&!interface_exists($class,false)&&!trait_exists($class,false))
+				throw new Exception('Class "'.$class.'" not found as expected in "'.$file.'"');
+			$this->checked[] = $class;
+			return true;
+		}
+		return false;
+	}
+	function findClass($class,$relative_class,$prefix){
+		if(isset($this->namespaces[$prefix])){
+			foreach($this->namespaces[$prefix] as $base_dir){
+				$file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
+				if($this->loadFile($file,$class))
+					return true;
+			}
+		}		
+	}
 	function classLoad($class){
 		if(in_array($class,$this->checked))
 			return;
@@ -18,13 +47,12 @@ class PSR4 extends Abstraction{
 				$prefix = '\\';
 				$relative_class = $class;
 			}
-			if(isset($this->namespaces[$prefix])){
-				foreach($this->namespaces[$prefix] as $base_dir){
-					$file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
-					if($this->loadFile($file,$class))
-						return;
-				}
-			}
+			if($this->findClass($class,$relative_class,$prefix))
+				return;
 		}
+		$this->extendSuperClass($class);
+	}
+	function __invoke($class){
+		return $this->classLoad($class);
 	}
 }
--- php/Autoload/SuperNamespace.php
+++ php/Autoload/SuperNamespace.php
@@ -1,41 +1,20 @@
 <?php namespace Surikat\Autoload;
 use Exception;
-use Surikat\Autoload\Abstraction;
-require_once __DIR__.'/Abstraction.php';
-class SuperNamespace extends Abstraction{
+use Surikat\Autoload\PSR4;
+require_once __DIR__.'/PSR4.php';
+class SuperNamespace extends PSR4{
 	protected $superNamespaces = [];
-	function classLoad($class){
-		if(in_array($class,$this->checked))
-			return;
-		$prefix = $class;
-		while($prefix!='\\'){
-			$prefix = rtrim($prefix, '\\');
-			$pos = strrpos($prefix, '\\');
-			if($pos!==false){
-				$prefix = substr($class, 0, $pos + 1);
-				$relative_class = substr($class, $pos + 1);
-				if(isset($this->superNamespaces[$prefix])){
-					foreach($this->superNamespaces[$prefix] as $base_dir){
-						$file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
-						if($this->loadFile($file,$class))
-							return;
-					}
-					return;
-				}
+	function findClass($class,$relative_class,$prefix){
+		if(parent::findClass($class,$relative_class,$prefix))
+			return true;
+		if($prefix!='\\'&&isset($this->superNamespaces[$prefix])){
+			foreach($this->superNamespaces[$prefix] as $base_dir){
+				$file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
+				if($this->loadFile($file,$class))
+					return true;
 			}
-			else{
-				$prefix = '\\';
-				$relative_class = $class;
-			}
-			if(isset($this->namespaces[$prefix])){
-				foreach($this->namespaces[$prefix] as $base_dir){
-					$file = $base_dir.str_replace('\\', '/', $relative_class).'.php';
-					if($this->loadFile($file,$class))
-						return;
-				}
-			}
+			return true;
 		}
-		$this->extendSuperClass($class);
 	}
 	function addSuperNamespace($prefix, $base_dir, $prepend = false){
 		$prefix = trim($prefix, '\\').'\\';
