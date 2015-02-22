3f1efbdf70ef8d67ac692fb9a1c31030ee770c71
surikat <jo@surikat.pro>
2015-02-23 00:46:21 +0100

    wip

--- php/User/SessionHandlerInterface.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Surikat\User;
-interface SessionHandlerInterface extends \SessionHandlerInterface{
-	function setKey($key);
-	function destroyKey($key);
-	function regenerateId();
-	function generateId();
-}
