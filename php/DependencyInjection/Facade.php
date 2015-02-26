53786ed32dea2dc5ea3f142b1c32b17506e278f8
surikat <jo@surikat.pro>
2015-02-27 00:45:19 +0100

    improve registry

--- php/DependencyInjection/Facade.php
+++ php/DependencyInjection/Facade.php
@@ -11,9 +11,12 @@ trait Facade{
 	static function setStatic(){
 		return static::$__instance = static::registry(func_get_args());
 	}
-	static function registry($args=null){
+	static function registry($args=null,$class=null){
 		$key = empty($args)?0:sha1(serialize($args));
-		$class = get_called_class();
+		if(!isset($class))
+			$class = get_called_class();
+		else
+			$key = $class.'.'.$key;
 		if(!isset(static::$__instances[$key])){
 			if(is_array($args)&&!empty($args))
 				static::$__instances[$key] = (new ReflectionClass($class))->newInstanceArgs($args);
