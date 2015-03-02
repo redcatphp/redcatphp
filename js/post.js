37105ad5f56aa41fbd300c22926f6fad017ff060
surikat <jo@surikat.pro>
2015-03-02 23:44:11 +0100

    add post simulator helper

---
+++ js/post.js
@@ -0,0 +1,16 @@
+function post(path, params){
+	var form = document.createElement("form");
+	form.setAttribute("method", 'POST');
+	form.setAttribute("action", path);
+	for(var key in params){
+		if(params.hasOwnProperty(key)) {
+			var hiddenField = document.createElement("input");
+			hiddenField.setAttribute("type", "hidden");
+			hiddenField.setAttribute("name", key);
+			hiddenField.setAttribute("value", params[key]);
+			form.appendChild(hiddenField);
+		 }
+	}
+	document.body.appendChild(form);
+	form.submit();
+}
