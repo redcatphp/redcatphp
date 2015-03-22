07f4514a18da9da53f0b902fcf4c610d9e064f70
surikat <jo@surikat.pro>
2015-03-23 00:50:47 +0100

    clean

--- php/bootstrap.php
+++ php/bootstrap.php
@@ -1,12 +1,4 @@
-<?php namespace Surikat;
-
-error_reporting(-1);
-ini_set('display_startup_errors',true);
-ini_set('display_errors','stdout');
-
-use Surikat\DependencyInjection\Container;
-use Surikat\Autoload\Psr4;
-
+<?php
 require __DIR__.'/Component/DependencyInjection/Mutator.php';
 require __DIR__.'/Component/DependencyInjection/MutatorMagicProperty.php';
 require __DIR__.'/Component/DependencyInjection/MutatorProperty.php';
@@ -15,13 +7,10 @@ require __DIR__.'/Component/DependencyInjection/MutatorCall.php';
 require __DIR__.'/Component/DependencyInjection/MutatorMagic.php';
 require __DIR__.'/Component/DependencyInjection/Registry.php';
 require __DIR__.'/Component/DependencyInjection/Container.php';
-
 require __DIR__.'/Component/Autoload/Psr4.php';
-
 require __DIR__.'/constants.php';
-
 global $SURIKAT;
-$SURIKAT = Container::get();
+$SURIKAT = Surikat\DependencyInjection\Container::get();
 $SURIKAT->Autoload_Psr4([
 	''			=> getcwd().'/php',
 	'Surikat'	=> __DIR__.'/Component',
