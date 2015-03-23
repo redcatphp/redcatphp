bba8e456ddbb2144efbb7255e4a8a6d1b47a087b
surikat <jo@surikat.pro>
2015-03-23 22:34:46 +0100

    improve

--- php/Component/DependencyInjection/MutatorMagicCall.php
+++ php/Component/DependencyInjection/MutatorMagicCall.php
@@ -8,11 +8,11 @@ trait MutatorMagicCall{
 			else
 				return $this->getDependency($f,$args);
 		}
-		elseif(is_callable('parent::__call')){
-			return parent::__call($f,$args);
-		}
 		elseif(method_exists($this,'___call')){
 			return $this->___call($f,$args);
+		}
+		elseif(is_callable('parent::__call')){
+			return parent::__call($f,$args);
 		}
 		else{
 			throw new BadMethodCallException(sprintf('Call to undefined method %s::%s()',get_class($this),$f));
--- php/Component/DependencyInjection/MutatorMagicProperty.php
+++ php/Component/DependencyInjection/MutatorMagicProperty.php
@@ -7,17 +7,19 @@ trait MutatorMagicProperty{
 				$r = $this->treeDependency($k);
 			else
 				$r = $this->getDependency($k);
-			return $r;
+		}
+		elseif(method_exists($this,'___get')){
+			$r = $this->___get($k);
 		}
 		elseif(is_callable('parent::__get')){
 			$r = parent::__get($k);
-			return $r;
 		}
 		else{
 			if(!isset($this->__legacyRegistry[$k]))
 				$this->__legacyRegistry[$k] = null;
-			return $this->__legacyRegistry[$k];
+			$r = $this->__legacyRegistry[$k];
 		}
+		return $r;
 	}
 	function __set($k,$v){
 		if(ctype_upper($k{0})||($k{0}=='_'&&ctype_upper($k{1}))){
@@ -25,6 +27,9 @@ trait MutatorMagicProperty{
 				$this->treeDependency($k,null,$v);
 			else
 				$this->setDependency($k,$v);
+		}
+		elseif(method_exists($this,'___set')){
+			$this->___set($k,$v);
 		}
 		elseif(is_callable('parent::__set')){
 			parent::__set($k,$v);
