156dbf90ee18dd52c364a3dad2503682950d00eb
surikat <jo@surikat.pro>
2015-03-23 02:17:38 +0100

    wip

--- php/bootstrap-3rd.php
+++ php/bootstrap-3rd.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__.'/bootstrap.php';
 $SURIKAT->Autoload_Psr4
-	->addNamespace('Zend',SURIKAT_SPATH.'php-3rd/Zend')
-	->addNamespace('Symfony',SURIKAT_SPATH.'php-3rd/Symfony')
+	->addNamespace('Zend',__DIR__.'/ThirdParty/Zend')
+	->addNamespace('Symfony',__DIR__.'/ThirdParty/Symfony')
 ;
