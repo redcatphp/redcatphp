e3a2a16087770a190771314ba8a39c84bd90655c
surikat <jo@surikat.pro>
2015-04-09 21:56:58 +0200

    wip

---
+++ php/Component/I18n/Translator.inc.php
@@ -0,0 +1,8 @@
+<?php namespace;
+use Surikat\Component\I18n\Translator;
+function __($msgid,$lang=null,$domain=null){
+	return Translator::__($msgid,$lang,$domain);
+}
+function n__($singular,$plural,$number,$lang=null,$domain=null){
+	return Translator::n__($singular, $plural, $number,$lang,$domain);
+}
