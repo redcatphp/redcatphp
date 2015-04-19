d2a9d1e735747dc25cecd0e8f7384ce87f9a6474
surikat <jo@surikat.pro>
2015-04-19 16:53:04 +0200

    improve

--- php/Component/Config/DependencyInjection.php
+++ php/Component/Config/DependencyInjection.php
@@ -2,39 +2,35 @@
 namespace Surikat\Component\Config;
 class DependencyInjection{
 	public $dirPath = 'config';
+	function getPath($c,$args=null){
+		$path = $this->dirPath.'/';
+		if(method_exists($c,'getConfigFilename')){
+			$path .= $c::getConfigFilename($args);
+		}
+		else{
+			$path .= str_replace('\\','_',$c);
+		}
+		$path .= '.php';
+		return $path;
+	}
 	function objectFactory($c,$args,$new,$mutator){
-		if(method_exists($c,'setConfig')){
-			$inc = $this->dirPath.'/';
-			if(method_exists($c,'getConfigFilename')){
-				$inc .= $c::getConfigFilename();
+		$path = $this->getPath($c,$args);
+		if(!method_exists($c,'setConfig')||!is_file($path))
+			return $mutator->makeDependency($c,$args,$new);
+		$config = $this->includeFree($path,$args);
+		if($config instanceof ConfigMethods){
+			if(isset($config['__construct'])){
+				$args = array_merge($config['__construct'],(array)$args);
+				unset($config['__construct']);
 			}
-			else{
-				$inc .= str_replace('\\','_',$c);
-			}
-			$inc .= '.php';
-			if(is_file($inc)){
-				$config = $this->includeFree($inc,$args);
-				if($config instanceof ConfigMethods){
-					if(isset($config['__construct'])){
-						$args = array_merge($config['__construct'],(array)$args);
-						unset($config['__construct']);
-					}
-					$obj = $mutator->makeDependency($c,$args,$new);
-					foreach($config as $method=>$arg){
-						$obj->$method($arg);
-					}
-				}
-				else{
-					$obj = $mutator->makeDependency($c,$args,$new);
-					$obj->setConfig($config);
-				}
-			}
-			else{
-				$obj = $mutator->makeDependency($c,$args,$new);
+			$obj = $mutator->makeDependency($c,$args,$new);
+			foreach($config as $method=>$arg){
+				$obj->$method($arg);
 			}
 		}
 		else{
 			$obj = $mutator->makeDependency($c,$args,$new);
+			$obj->setConfig($config);
 		}
 		return $obj;
 	}
