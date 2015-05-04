f47b67ea3e50a00201613845b4ae05b2ec9772b4
surikat <jo@surikat.pro>
2015-05-04 22:49:45 +0200

    wip

--- php/Config/ConfigMethods.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Config;
-class ConfigMethods extends \ArrayObject implements \ArrayAccess{
-	function __construct($a=[]){
-		parent::setFlags(parent::ARRAY_AS_PROPS);
-		parent::__construct((array)$a);
-	}
-}
