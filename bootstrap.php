f40fcb5282a82738fc52f39eed83639f46eeeaf4
surikat <jo@surikat.pro>
2015-05-18 09:08:14 +0200

    wip

--- bootstrap.php
+++ bootstrap.php
@@ -2,12 +2,12 @@
 error_reporting(-1);
 ini_set('display_startup_errors',true);
 ini_set('display_errors','stdout');
-require __DIR__.'/Unit/AutoloadPsr4.php';
+require __DIR__.'/php/Unit/AutoloadPsr4.php';
 define('SURIKAT',realpath(__DIR__.'/..').'/');
 define('SURIKAT_CWD',getcwd().'/');
 global $SURIKAT;
 $SURIKAT['Autoloader'] = new Unit\AutoloadPsr4();
 $SURIKAT['Autoloader']->addNamespace('',[
 	getcwd().'/php',
-	__DIR__
+	__DIR__.'/php'
 ])->splRegister();
