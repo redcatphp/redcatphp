3365946e6e050c8cde9586de4beb029140fcfd9f
surikat <jo@surikat.pro>
2015-05-20 14:47:29 +0200

    wip

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,19 @@
 <?php
-error_reporting(-1);
-ini_set('display_startup_errors',true);
-ini_set('display_errors','stdout');
+//error_reporting(-1);
+//ini_set('display_startup_errors',true);
+//ini_set('display_errors','stdout');
 
-define('SURIKAT',realpath(__DIR__.'/..').'/');
+define('SURIKAT',__DIR__.'/');
 define('SURIKAT_CWD',getcwd().'/');
 
 require __DIR__.'/php/Unit/Autoloader.php';
 Unit\Autoloader::getInstance()->addNamespace('',[
-		getcwd().'/php',
-		__DIR__.'/php'
+		SURIKAT_CWD.'php',
+		SURIKAT.'php'
 ])->splRegister();
 
 global $SURIKAT;
 $SURIKAT = new Unit\DiContainer;
 
+$SURIKAT->loadXml(SURIKAT.'.config.xml');
+$SURIKAT->loadXml(SURIKAT_CWD.'.config.xml');
