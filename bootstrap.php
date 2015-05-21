fb21f9301c2a34a2a41b6d0522c9d5f2c41877d1
surikat <jo@surikat.pro>
2015-05-21 10:35:26 +0200

    wip

--- bootstrap.php
+++ bootstrap.php
@@ -6,14 +6,13 @@
 define('SURIKAT',__DIR__.'/');
 define('SURIKAT_CWD',getcwd().'/');
 
-require __DIR__.'/php/Unit/Autoloader.php';
+require_once __DIR__.'/php/Unit/Autoloader.php';
 Unit\Autoloader::getInstance()->addNamespace('',[
 		SURIKAT_CWD.'php',
 		SURIKAT.'php'
 ])->splRegister();
 
 global $SURIKAT;
-$SURIKAT = new Unit\DiContainer;
-
+$SURIKAT = Unit\DiContainer::getInstance();
 $SURIKAT->loadXml(SURIKAT.'.config.xml');
 $SURIKAT->loadXml(SURIKAT_CWD.'.config.xml');
