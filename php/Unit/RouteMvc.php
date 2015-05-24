3b3b4aae823d944dcf381391545321c12cf1886d
surikat <jo@surikat.pro>
2015-05-24 20:46:09 +0200

    wip

--- php/Unit/RouteMvc.php
+++ php/Unit/RouteMvc.php
@@ -1,19 +1,24 @@
 <?php namespace Unit;
-class Route {
+class RouteMvc {
+	private $model;
 	private $view;
     private $controller;
     private $di;
-    function __construct($view, $controller = null, DiContainer $di){
+    function __construct($model, $view, $controller = null, DiContainer $di){
+        $this->model = $model;
         $this->view = $view;
         $this->controller = $controller;
         $this->di = $di;
     }
     function __invoke($params){
+		if(is_string($this->model)){
+			$this->model = $this->di->create($this->model);
+		}
 		if(is_string($this->view)){
-			$this->view = $this->di->create($this->view);
+			$this->view = $this->di->create($this->view,[$this->model]);
 		}
 		if(is_string($this->controller)){
-			$this->controller = $this->di->create($this->controller);
+			$this->controller = $this->di->create($this->controller,[$this->model]);
 		}
 		if($this->controller){
 			call_user_func($this->controller,$params);
