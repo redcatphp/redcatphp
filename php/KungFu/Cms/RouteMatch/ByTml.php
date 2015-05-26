69029fcd08d63566238e9100a53ea5b3c9a61cc1
surikat <jo@surikat.pro>
2015-05-26 23:35:20 +0200

    fix

--- php/KungFu/Cms/RouteMatch/ByTml.php
+++ php/KungFu/Cms/RouteMatch/ByTml.php
@@ -2,6 +2,7 @@
 class ByTml {
 	protected $uriDir;
 	protected $physicalDir;
+	protected $extension;
 	function __construct($uriDir=null,$physicalDir=null,$extension='.tml'){
 		$this->uriDir = $uriDir;
 		$this->physicalDir = isset($physicalDir)?$physicalDir:$uriDir;
@@ -12,8 +13,8 @@ class ByTml {
 			return;
 		if($this->uriDir)
 			$uri = substr($uri,strlen($this->uriDir));
-		$file = $this->physicalDir.'/'.ltrim($uri,'/').$extension;
+		$file = $this->physicalDir.'/'.ltrim($uri,'/').$this->extension;
 		if(is_file($file))
-			return [$this->physicalDir,$uri.$extension];
+			return [$this->physicalDir,$uri.$this->extension];
 	}
 }
