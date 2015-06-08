760b00c9b8030fc788b7ad7bea4c854fbc59efab
surikat <jo@surikat.pro>
2015-06-08 09:01:21 +0200

    add params to DiExpand

--- php/Unit/DiExpand.php
+++ php/Unit/DiExpand.php
@@ -2,13 +2,14 @@
 namespace Unit;
 class DiExpand{
 	private $x;
-	function __construct($x){
+	function __construct($x,$params=[]){
 		$this->x = $x;
+		$this->params = $params;
 	}
 	function __invoke(Di $di, $share = []){
 		if(is_string($this->x))
-			return $di->create($this->x,[],false,$share);
+			return $di->create($this->x,$this->params,false,$share);
 		else
-			return call_user_func($this->x);
+			return call_user_func_array($this->x,$this->params);
 	}
 }
