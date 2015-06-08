0ce3be4f5398f4f54f4f5940f74106a03c6e5e57
surikat <jo@surikat.pro>
2015-06-08 11:45:19 +0200

    improve + fixed primary by repository

--- php/RedBase/Maphper/Repository.php
+++ php/RedBase/Maphper/Repository.php
@@ -1,18 +1,44 @@
 <?php
 namespace RedBase\Maphper;
-class Repository{
-	protected $factory;
+class Repository implements \ArrayAccess{
+	private $factory;
+	private $primary;
 	private $registry = [];
-	function __construct($factory){
+	function __construct($factory,$primary='id'){
 		$this->factory = $factory;
+		$this->primary = $primary;
 	}
-	function create($table, $primary='id'){
-		return new Maphper(call_user_func($this->factory,$table,$primary));
+	function getPrimaryKey(){
+		return $this->primary;
 	}
-	function get($table,$primary='id'){
-		if(!isset($this->registry[$table][$primary]))
-			$this->registry[$table][$primary] = $this->create($table,$primary);
-		return $this->registry[$table][$primary];
+	private function create($table){
+		return new Maphper(call_user_func($this->factory,$table,$this->primary));
 	}
-	
+	function __isset($k){
+		return $this->offsetExists($k);
+	}
+	function __get($k){
+		return $this->offsetGet($k);
+	}
+	function __set($k,$v){
+		$this->offsetSet($k,$v);
+	}
+	function __unset($k){
+		$this->offsetUnset($k);
+	}
+	function offsetExists($k){
+		return isset($this->registry[$k]);
+	}
+	function offsetGet($k){
+		if(!isset($this->registry[$k]))
+			$this->registry[$k] = $this->create($k);
+		return $this->registry[$k];
+	}
+	function offsetSet($k,$v){
+		$this->registry[$k] = $v;
+	}
+	function offsetUnset($k){
+		if(isset($this->registry[$k]))
+			unset($this->registry[$k]);
+	}
 }
--- php/RedBase/Maphper/RepositoryPDO.php
+++ php/RedBase/Maphper/RepositoryPDO.php
@@ -3,13 +3,12 @@ namespace RedBase\Maphper;
 class RepositoryPDO extends Repository{
 	private $pdo;
 	private $options;
-	protected $factory;
-	function __construct($dsn,$user=null,$password=null,$options=null){
+	function __construct($dsn,$user=null,$password=null,$primary='id',$options=null){
+		parent::__construct([$this,'dbFactory'],$primary);
 		$this->pdo = new \PDO($dsn,$user,$password);
-		$this->factory = [$this,'dbFactory'];
 		$this->options = $options;
 	}
-	function dbFactory($table,$primary){
-		return new DataSource\Database($this->pdo, $table, $primary, $this->options);
+	function dbFactory($table){
+		return new DataSource\Database($this->pdo, $table, $this->getPrimaryKey(), $this->options);
 	}
 }
