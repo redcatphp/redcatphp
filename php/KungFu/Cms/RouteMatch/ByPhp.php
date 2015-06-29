095ef6bf5cf5d348fc2b1c5483378997b3a59ef1
surikat <jo@surikat.pro>
2015-06-29 10:49:20 +0200

    universalize

--- php/KungFu/Cms/RouteMatch/ByPhp.php
+++ php/KungFu/Cms/RouteMatch/ByPhp.php
@@ -1,39 +1,4 @@
 <?php namespace KungFu\Cms\RouteMatch;
-class ByPhp{
-	protected $dir = '';
-	protected $dirFS = '';
+class ByPhp extends \Unit\RouteMatch\ByPhp{
 	protected $dirs = ['','Surikat/'];
-	function __construct($dir=null,$dirFS=null){
-		if(isset($dir)||isset($dirFS)){
-			if(!isset($dirFS))
-				$dirFS = $dir;
-			$this->dir = $dir;
-			$this->dirFS = $dirFS;
-		}
-	}
-	function __invoke($uri){
-		if($this->dir&&strpos($uri,$this->dir)!==0)
-			return;
-		$path = ltrim(substr($uri,strlen($this->dir)),'/');
-		if(!$path||substr($path,-1)=='/')
-			$path .= 'index.php';
-		$file = $this->dirFS.'/'.$path;
-		foreach($this->dirs as $d){
-			if($f=realpath(($adir=$d.'/').$file))
-				return [$this->dirFS,$path,$adir.$this->dirFS,$f];
-		}
-	}
-	function setDirs($d){
-		$this->dirs = (array)$d;
-		foreach($this->dirs as $d){
-			if($d)
-				$this->dirs[$k] = rtrim($d,'/').'/';
-		}
-	}
-	function prependDir($d){
-		array_unshift($this->dirs,$d?rtrim($d,'/').'/':'');
-	}
-	function appendDir($d){
-		$this->dirs[] = $d?rtrim($d,'/').'/':'';
-	}
 }
