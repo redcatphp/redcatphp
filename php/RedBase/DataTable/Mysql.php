f4d6f91a7aedc7a27ec228a90058f6b261a832cc
surikat <jo@surikat.pro>
2015-07-23 20:56:11 +0200

    add fullTextSearchInnoDB

--- php/RedBase/DataTable/Mysql.php
+++ php/RedBase/DataTable/Mysql.php
@@ -1,5 +1,17 @@
 <?php
 namespace RedBase\DataTable;
 class Mysql extends SQL{
-	
+	function fullTextSearch($text){
+		if($this->dataSource->fulltextAvailableOnInnoDB())
+			return call_user_func_array([$this,'fullTextSearchInnoDB'],func_get_args());
+		else
+			return call_user_func_array([$this,'fullTextSearchMyISAM'],func_get_args());
+	}
+	function fullTextSearchInnoDB($text,$columns=[]){
+		$this->dataSource->addFtsIndex($this->name,$columns,$this->primaryKey,$this->uniqTextKey,$this->fullTextSearchLocale);
+		$this->where('MATCH (`'.implode('`,`',$columns).'`) AGAINST (?)',[$text]);
+	}
+	function fullTextSearchMyISAM($text){
+		
+	}
 }
