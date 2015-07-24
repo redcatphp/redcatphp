edfecd8ec89268cfecb6b00be0d0c41d7f27f0ca
surikat <jo@surikat.pro>
2015-07-24 17:28:36 +0200

    add fullTextSearchMyISAM

--- php/RedBase/DataTable/Mysql.php
+++ php/RedBase/DataTable/Mysql.php
@@ -1,13 +1,7 @@
 <?php
 namespace RedBase\DataTable;
 class Mysql extends SQL{
-	function fullTextSearch($text){
-		if($this->dataSource->fulltextAvailableOnInnoDB())
-			return call_user_func_array([$this,'fullTextSearchInnoDB'],func_get_args());
-		else
-			return call_user_func_array([$this,'fullTextSearchMyISAM'],func_get_args());
-	}
-	function fullTextSearchInnoDB($text,$mode='',$columns=[]){
+	function fullTextSearch($text,$mode='',$columns=[]){
 		if($mode){
 			switch(strtoupper($mode)){
 				case 'EXP':
@@ -26,8 +20,14 @@ class Mysql extends SQL{
 				break;
 			}
 		}
+		if($this->dataSource->fulltextAvailableOnInnoDB())
+			$this->fullTextSearchInnoDB($text,$mode,$columns);
+		else
+			$this->fullTextSearchMyISAM($text,$mode,$columns);
+	}
+	function fullTextSearchInnoDB($text,$mode='',&$columns=[]){
 		$table = $this->dataSource->escTable($this->name);
-		$this->dataSource->addFtsIndex($this->name,$columns,$this->primaryKey,$this->uniqTextKey,$this->fullTextSearchLocale);
+		$this->dataSource->addFtsIndex($this->name,$columns,$this->primaryKey,$this->uniqTextKey);
 		$cols = '`'.implode('`,`',$columns).'`';
 		$this->where('MATCH('.$cols.') AGAINST (? '.$mode.')',[$text]);
 		$this->select('MATCH('.$cols.') AGAINST (? '.$mode.') AS _rank',[$text]);
@@ -37,7 +37,30 @@ class Mysql extends SQL{
 			return $this->dataSource->getCell('SELECT COUNT(IF(MATCH ('.$cols.') AGAINST (?), 1, NULL)) FROM '.$table,[$text]);
 		});
 	}
-	function fullTextSearchMyISAM($text){
-		
+	function fullTextSearchMyISAM($text,$mode='',&$columns=[]){
+		$table = $this->dataSource->escTable($this->name);
+		$ftsTable = $this->dataSource->escTable($this->name.$this->dataSource->getFtsTableSuffix());
+		$this->dataSource->makeFtsTableAndIndex($this->name,$columns,$this->primaryKey,$this->uniqTextKey);
+		$cols = '`'.implode('`,`',$columns).'`';
+		$pk = $this->dataSource->esc($this->primaryKey);
+		$this->select("$table.*");
+		$this->unFrom($table);
+		$limit = $this->getLimit();
+		$offset = $this->getOffset();
+		if($limit)
+			$limit = 'LIMIT '.$limit;
+		if($offset)
+			$offset = 'OFFSET '.$offset;
+		$this->join("(
+			SELECT $ftsTable.$pk, MATCH($cols) AGAINST(? $mode) AS rank
+				FROM $ftsTable
+				WHERE MATCH($cols) AGAINST(? $mode)
+				ORDER BY rank DESC
+				$limit $offset
+		) AS _ranktable ON _ranktable.$pk = $table.$pk",[$text,$text]);
+		$this->orderBy('_ranktable.rank DESC');
+		$this->setCounter(function()use($cols,$ftsTable,$text){
+			return $this->dataSource->getCell('SELECT COUNT(IF(MATCH ('.$cols.') AGAINST (?), 1, NULL)) FROM '.$ftsTable,[$text]);
+		});
 	}
 }
