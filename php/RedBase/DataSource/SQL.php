97d9576aaf47f535fbd20bdfeab05ce833191d82
surikat <jo@surikat.pro>
2015-07-26 15:53:41 +0200

    log resultset via iterator cursor

--- php/RedBase/DataSource/SQL.php
+++ php/RedBase/DataSource/SQL.php
@@ -229,6 +229,7 @@ abstract class SQL extends DataSource{
 				$fetchStyle = ( isset( $options['fetchStyle'] ) ) ? $options['fetchStyle'] : NULL;
 				if ( isset( $options['noFetch'] ) && $options['noFetch'] ) {
 					$this->resultArray = [];
+					$this->logger->log('resultset via iterator cursor: '.$this->affectedRows.' rows');
 					return $statement;
 				}
 				$this->resultArray = $statement->fetchAll( $fetchStyle );
