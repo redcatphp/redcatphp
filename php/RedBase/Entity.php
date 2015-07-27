6dd53af9f23bf1708f0dc344cea81cc67bf8362a
surikat <jo@surikat.pro>
2015-07-27 23:26:33 +0200

    clean from dev

--- php/RedBase/Entity.php
+++ php/RedBase/Entity.php
@@ -1,49 +1,15 @@
 <?php
 namespace RedBase;
 class Entity implements Observer{
-	//function beforeRecursive(){}
-	//function beforePut(){}
-	//function beforeCreate(){}
-	//function beforeRead(){}
-	//function beforeUpdate(){}
-	//function beforeDelete(){}
-	//function afterPut(){}
-	//function afterCreate(){}
-	//function afterRead(){}
-	//function afterUpdate(){}
-	//function afterDelete(){}
-	
-	function beforeRecursive(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function beforePut(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function beforeCreate(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function beforeRead(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function beforeUpdate(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function beforeDelete(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function afterPut(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function afterCreate(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function afterRead(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function afterUpdate(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
-	function afterDelete(){
-		echo 'called '.$this->_type.':'.__FUNCTION__.'()<br>';
-	}
+	function beforeRecursive(){}
+	function beforePut(){}
+	function beforeCreate(){}
+	function beforeRead(){}
+	function beforeUpdate(){}
+	function beforeDelete(){}
+	function afterPut(){}
+	function afterCreate(){}
+	function afterRead(){}
+	function afterUpdate(){}
+	function afterDelete(){}
 }
