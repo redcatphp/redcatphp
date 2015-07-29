c840c8ec4ad3535b455756bc928e52bda0efcbb4
surikat <jo@surikat.pro>
2015-07-29 23:06:44 +0200

    add markup video plugin

---
+++ php/KungFu/TemplixPlugin/Markup/Video.php
@@ -0,0 +1,14 @@
+<?php
+namespace KungFu\TemplixPlugin\Markup;
+class Video extends \Templix\Markup{
+	function load(){
+		if($this->source){
+			$this->prepend('
+				<source src="'.$this->source.'.mp4" type="video/mp4" />
+				<source src="'.$this->source.'.webm" type="video/webm" />
+				<source src="'.$this->source.'.ogv" type="video/ogg" />
+			');
+			unset($this->source);
+		}
+	}
+}
