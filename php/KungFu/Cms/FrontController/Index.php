26177456fe4eb61bc8cb6810cdc5e98eb2adbd0a
surikat <jo@surikat.pro>
2015-07-31 14:36:59 +0200

    clean

--- php/KungFu/Cms/FrontController/Index.php
+++ php/KungFu/Cms/FrontController/Index.php
@@ -7,13 +7,9 @@ class Index extends \Unit\FrontController{
 		parent::__construct($router,$di);
 		$this->map([
 			['backend/','new:KungFu\Cms\FrontController\Backoffice'],
-			['service/','new:KungFu\Cms\Service\Service'],
 			[['new:Unit\RouteMatch\Extension','css|js|png|jpg|jpeg|gif'],'new:KungFu\Cms\FrontController\Synaptic'],
-			//[['new:KungFu\Cms\RouteMatch\ByTml','plugin'],[$this,'template']],
-			//[['new:KungFu\Cms\RouteMatch\ByTml','surikat/plugin'],[$this,'template']],
 			[['new:KungFu\Cms\RouteMatch\ByTmlL10n','','template'],'new:KungFu\TemplixPlugin\TemplixL10n'],
 			[['new:KungFu\Cms\RouteMatch\ByTml','','template'],'new:KungFu\TemplixPlugin\Templix'],
-			//[['new:KungFu\Cms\RouteMatch\ByTml','surikat/template'],[$this,'template']],
 		]);
 	}
 	function run($path,$domain=null){
