cb0bb0576ab5143472a4abd99ab375a61470a71b
surikat <jo@surikat.pro>
2015-08-14 00:19:13 +0200

    improve js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,12 @@
-(function(w,d){	
-	var indexOf = Array.prototype.indexOf?function(a,obj, start){
+/*
+	$js - the lightest javascript dependency manager
+	@version 2.0
+	@link http://github.com/surikat/js/
+	@author Jo Surikat <jo@surikat.pro>
+	@website http://wildsurikat.com
+*/
+(function(w,d){
+	var indexOf = Array.prototype.indexOf?function(a,obj,start){
 		return a.indexOf(obj,start);
 	}:function(a,obj, start){
 		var j = a.length;
@@ -26,37 +33,7 @@
 	var required = [];
 	var handled = [];
 	var requiring = {};
-	var obj = {
-		dev:false,
-		async:true,
-		preloaders:[],
-		path:'js/',
-		pathDetection:true,
-		pathSuffix:'.js',
-		min:true,
-		config:function(k,v){
-			if(arguments.length==2){
-				obj[k] = v;
-			}
-			else if(arguments.length){
-				if(typeof(k)=='object'){
-					if(k instanceof Array)
-						for(var i=0;i<k.length;i++)
-							obj.config(k[i]);
-					else
-						for(var key in k)
-							obj.config(key,k[key]);
-				}
-				else
-					return obj[k];
-			}
-			else
-				return obj;
-		},
-		start: function(){
-			
-		}
-	};
+	
 	var wait = function(u){
 		if(indexOf(handled,u)>-1)
 			handle(u);
@@ -71,21 +48,24 @@
 				(requiring[u].shift())();
 	};
 	var getSrc = function(u){
-		return u?(obj.path&&u.indexOf('://')<0&&u.indexOf('/')!==0&&(!obj.pathDetection||u.indexOf(obj.path)!=0)?(obj.path+u):u)+(obj.pathSuffix&&u.indexOf('://')<0&&(!obj.pathDetection||u.substr(u.length-obj.pathSuffix.length)!=obj.pathSuffix)?obj.pathSuffix:''):u;
+		return u?($js.path&&u.indexOf('://')<0&&u.indexOf('/')!==0&&(!$js.pathDetection||u.indexOf($js.path)!=0)?($js.path+u):u)+($js.pathSuffix&&u.indexOf('://')<0&&(!$js.pathDetection||u.substr(u.length-$js.pathSuffix.length)!=$js.pathSuffix)?$js.pathSuffix:''):u;
 	};
 	var createScript = function(u){
 		var s = d.createElement('script');
 		d.type = 'text/javascript';
 		d.body.appendChild(s);
 		var callback = function(){
+			var shift = $js.modulesStack.shift();
+			if(shift)
+				$js.modules[u] = shift;
 			required.push(u);
 			handle(u);
 			handled.push(u);
 		};
 		s.onload = callback;
-		s.onreadystatechange = function(){if(callback&&(this.readyState=='complete'||this.readyState==='loaded')){callback();callback=null;}}; //old browsers
+		s.onreadystatechange = function(){if(callback&&this.readyState==='loaded'){callback();callback=null;}}; //old browsers
 		s.setAttribute('async','async');
-		s.src = cacheFix(u,obj.dev,obj.min,'js');
+		s.src = cacheFix(u,$js.dev,$js.min,'js');
 	};
 	var x = function(u,c){
 		if(!u){
@@ -137,8 +117,14 @@
 	var asyncJsObject = function(u,c){
 		if(c){
 			requiredGroups.push([]);
-			for(var k in u)
-				asyncArrayCall(u,u[k],c,requiredGroups.length-1);
+			var arr = [];
+			for(var k in u){
+				arr.push(k);
+				$js(k,u[k],1);
+			}
+			for(var k in u){
+				asyncArrayCall(arr,k,c,requiredGroups.length-1);
+			}
 		}
 		else{
 			for(var k in u)
@@ -159,8 +145,14 @@
 		}
 		else{
 			var ev = '';
-			for(var k in u.reverse())
+			var arr = [];
+			for(var key in u){
+				arr.push(key);
+			}
+			for(var key in arr.reverse()){
+				var k = arr[key];
 				ev = '$js("'+k+'"'+(ev?',function(){'+ev+'}':(u[k]?',u["'+k+'"]':''))+',1);';
+			}
 			eval(ev);
 		}
 	};
@@ -172,54 +164,144 @@
 		if(typeof(c)=='function')
 			scripts[m][u].push(c);
 	};
-	$js = function(){
-		if(!arguments.length)
-			return obj;
-		var u,
-			c,
-			sync = !obj.async;
-		for(var i in arguments){
-			switch(typeof(arguments[i])){
-				case 'boolean':
-					sync = arguments[i];
-				break;
-				case 'function':
-					c = arguments[i];
-				break;
-				case 'string':
-				case 'object':
-					u = arguments[i];
-				break;
+	$js = (function(){
+		var js = function(){
+			var u,
+				c,
+				sync = !$js.async;
+			for(var i = 0; i < arguments.length; i++){
+				switch(typeof(arguments[i])){
+					case 'boolean':
+						sync = arguments[i];
+					break;
+					case 'function':
+						c = arguments[i];
+					break;
+					case 'string':
+					case 'object':
+						u = arguments[i];
+					break;
+				}
 			}
-		}
-		if(typeof(u)=='object'){
-			if(u instanceof Array){
-				if(sync)
-					syncJsArray(u,c);
-				else
-					asyncJsArray(u,c);
+			if(typeof(u)=='string'){
+				if(typeof($js.aliasMap[u])!='undefined'){
+					u = $js.aliasMap[u];
+				}
+			}
+			else if(typeof(u)=='object'){
+				if(u instanceof Array){
+					var un = [];
+					for(var i in u){
+						if(typeof($js.aliasMap[u[i]])!='undefined'){
+							var alias = $js.aliasMap[u[i]];
+							if(typeof(alias)=='object'){
+								for(var ii in alias){
+									if(un.indexOf(alias[ii])===-1)
+										un.push(alias[ii]);
+								}
+							}
+							else{
+								if(un.indexOf(alias)===-1)
+									un.push(alias);
+							}
+						}
+						else{
+							if(un.indexOf(u[i])===-1)
+								un.push(u[i]);
+						}
+					}
+					u = un;
+				}
+				else{
+					for(var k in u){
+						if(typeof($js.aliasMap[k])!='undefined'){
+							var alias = $js.aliasMap[k];
+							var sc = u[k];
+							delete(u[k]);
+							if(typeof(alias)=='object'){
+								if(sync){
+									return $js(sync,alias,function(){
+										sc();
+										$js(sync,u,c);
+									});
+								}
+								else{
+									for(var ii in alias){
+										u[alias[ii]] = null;
+									}
+								}
+							}
+							else{
+								u[alias] = sc;
+							}
+						}
+					}
+				}
+			}
+			if(typeof(u)=='object'){
+				if(u instanceof Array){
+					if(sync)
+						syncJsArray(u,c);
+					else
+						asyncJsArray(u,c);
+				}
+				else{
+					if(sync)
+						syncJsObject(u,c);
+					else
+						asyncJsObject(u,c);
+				}
 			}
 			else{
-				if(sync)
-					syncJsObject(u,c);
-				else
-					asyncJsObject(u,c);
+				if(typeof(u)=='function'){
+					c = u;
+					u = 0;
+				}
+				apt(u,c,!sync);
 			}
+			return function(){
+				var a = arguments;
+				return $js(u,function(){
+					$js.apply(null,a);
+				});
+			};
+		};
+		js.dev = false;
+		js.async = true;
+		js.preloaders = [];
+		js.path = 'js/';
+		js.pathDetection = true;
+		js.pathSuffix = '.js';
+		js.min = false;
+		js.aliasMap = {};
+		js.alias = function(alias,concrete){
+			js.aliasMap[alias] = concrete;
+		};
+		
+		js.modules = {};
+		js.modulesStack = [];
+		var bases = document.getElementsByTagName('base');
+		var baseHref;
+		if(bases.length>0){
+			baseHref = bases[0].href.trim('/')+'/';
 		}
 		else{
-			if(typeof(u)=='function'){
-				c = u;
-				u = 0;
-			}
-			apt(u,c,!sync);
+			var path = window.location.pathname;
+			path = path.split('/');
+			path.pop();
+			path = path.join('/')+'/';
+			baseHref = window.location.protocol+"//"+window.location.hostname+(location.port?":"+location.port:'')+path;
 		}
-		return function(){
-			var a = arguments;
-			return $js(u,function(){
-				$js.apply(null,a);
-			});
+		js.module = function(mod){
+			if(typeof(mod)==='string'){
+				return js.modules[getSrc(mod)];
+			}
+			else{
+				js.modulesStack.push(mod);
+			}
 		};
-	};
+		return js;
+	})();
 	var y = {};
 	var loader = function(m,k){
 		var s = scripts[m][k];
@@ -251,61 +333,40 @@
 	};
 
 
-	var css = {
-		dev:false,
-		path:'css/',
-		pathDetection:true,
-		pathSuffix:'.css',
-		min:true,
-		config:function(k,v){
-			if(arguments.length==2){
-				obj[k] = v;
-			}
-			else if(arguments.length){
-				if(typeof(k)=='object'){
-					if(k instanceof Array)
-						for(var i=0;i<k.length;i++)
-							obj.config(k[i]);
-					else
-						for(var key in k)
-							obj.config(key,k[key]);
-				}
-				else
-					return obj[k];
-			}
-			else
-				return obj;
-		}
-	};
 	var getHref = function(u){
-		return u?(css.path&&u.indexOf('://')<0&&u.indexOf('/')!==0&&(!css.pathDetection||u.indexOf(css.path)!=0)?(css.path+u):u)+(css.pathSuffix&&u.indexOf('://')<0&&(!css.pathDetection||u.substr(u.length-css.pathSuffix.length)!=css.pathSuffix)?css.pathSuffix:''):u;
+		return u?($css.path&&u.indexOf('://')<0&&u.indexOf('/')!==0&&(!$css.pathDetection||u.indexOf($css.path)!=0)?($css.path+u):u)+($css.pathSuffix&&u.indexOf('://')<0&&(!$css.pathDetection||u.substr(u.length-$css.pathSuffix.length)!=$css.pathSuffix)?$css.pathSuffix:''):u;
 	};
 	var loadedCSS = [];
-	$css = function(fileName, media){
-		if(typeof(fileName)=='undefined')
-			return css;
-		var test = fileName;
-		fileName = getHref(fileName);
-		if(indexOf(loadedCSS,fileName)<0){
-			loadedCSS.push(fileName);
-			var links = d.getElementsByTagName('link'), i = links.length, style;
-			var exist = false;
-			while(i--){ // check if not already loaded fixed in head
-				if (links[i].href.indexOf(fileName) > -1)
-					exist = true;
+	$css = (function(){
+		var css = function(fileName, media){
+			var test = fileName;
+			fileName = getHref(fileName);
+			if(indexOf(loadedCSS,fileName)<0){
+				loadedCSS.push(fileName);
+				var links = d.getElementsByTagName('link'), i = links.length, style;
+				var exist = false;
+				while(i--){ // check if not already loaded fixed in head
+					if (links[i].href.indexOf(fileName) > -1)
+						exist = true;
+				}
+				if(!exist){
+					style = d.createElement('link');
+					style.type = 'text/css';
+					style.rel = 'stylesheet';
+					if(media)
+						style.media =  media;
+					style.href = cacheFix(fileName,$css.dev,$css.min,'css');
+					d.getElementsByTagName('head')[0].appendChild(style);
+				}
 			}
-			if(!exist){
-				style = d.createElement('link');
-				style.type = 'text/css';
-				style.rel = 'stylesheet';
-				if(media)
-					style.media =  media;
-				style.href = cacheFix(fileName,css.dev,css.min,'css');
-				d.getElementsByTagName('head')[0].appendChild(style);
-			}
-		}
+		};
+		css.dev = false;
+		css.path = 'css/';
+		css.pathDetection = true;
+		css.pathSuffix = '.css';
+		css.min = false;
 		return css;
-	};
+	})();
 
 	w.getElementsByXDom = function(){
 		var elArray = [];
@@ -319,8 +380,8 @@
 	
 	var load = function(){			
 		apt = x;
-		for(var i=0;i<obj.preloaders.length;i++){
-			obj.preloaders[i]();
+		for(var i=0;i<$js.preloaders.length;i++){
+			$js.preloaders[i]();
 		}
 		for(var k in scripts[0])
 			loader(0,k);
