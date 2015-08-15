baeb154eb7c5bcde20d3227f18911b4418d58d0c
surikat <jo@surikat.pro>
2015-08-15 23:54:19 +0200

    apply html5 implicit convention

--- php/Templix/MarkupHtml5/Script.php
+++ php/Templix/MarkupHtml5/Script.php
@@ -2,4 +2,8 @@
 namespace Templix\MarkupHtml5;
 class Script extends \Templix\Markup{
 	protected $noParseContent = true;
+	function load(){
+		if(!isset($this->type))
+			$this->type = 'text/javascript';
+	}
 }
