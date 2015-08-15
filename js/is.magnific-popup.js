05713c177835dedcdeea788bd3d804a0ea2902f3
surikat <jo@surikat.pro>
2015-08-15 23:49:55 +0200

    clean js path

--- js/is.magnific-popup.js
+++
@@ -1,4 +0,0 @@
-$css('is.magnific-popup');
-$js(['jquery','magnific-popup'],true,function(){
-	$('[is="magnific-popup"]').magnificPopup({type:'image'});
-});
--- js/jquery-ui/effect-fade.js
+++
@@ -1,1 +0,0 @@
-(function(t){t.effects.effect.fade=function(e,i){var s=t(this),n=t.effects.setMode(s,e.mode||"toggle");s.animate({opacity:n},{queue:!1,duration:e.duration,easing:e.easing,complete:i})}})(jQuery);
