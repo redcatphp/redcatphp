17d4bcf4b972ecbb7f9abb64104a2b516295d296
surikat <jo@surikat.pro>
2015-09-18 01:56:14 +0200

    fix up

--- php/Wild/Plugin/Templix/Toolbox.php
+++ php/Wild/Plugin/Templix/Toolbox.php
@@ -39,12 +39,12 @@ class Toolbox{
 			$head->append('<link href="'.$href.strtolower($is).'.css" rel="stylesheet" type="text/css">');
 	}
 	function autoMIN($Tml){
-		if(!$Tml->templix&&$Tml->templix->devCss){
+		if($Tml->templix&&!$Tml->templix->devCss){
 			foreach($Tml('link[href][rel=stylesheet],link[href][type="text/css"]') as $l)
 				if(strpos($l,'://')===false)
 					$l->href = (strpos($l->href,'/')!==false?dirname($l->href).'/':'').pathinfo($l->href,PATHINFO_FILENAME).'.min.'.pathinfo($l->href,PATHINFO_EXTENSION);
 		}
-		if(!$Tml->templix&&$Tml->templix->devJs){
+		if($Tml->templix&&!$Tml->templix->devJs){
 			foreach($Tml('script[src]') as $s)
 				if(strpos($s->src,'://')===false&&substr($s->src,-8)!='.pack.js')
 					$s->src = (strpos($s->src,'/')!==false?dirname($s->src).'/':'').pathinfo($s->src,PATHINFO_FILENAME).'.min.'.pathinfo($l->src,PATHINFO_EXTENSION);
