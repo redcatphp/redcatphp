2a93d14e0560004abe065fe2cb181af05594fe88
surikat <jo@surikat.pro>
2015-09-19 12:45:35 +0200

    fix autoloaders

--- php/Wild/Route/autoload.inc.php
+++ php/Wild/Route/autoload.inc.php
@@ -2,7 +2,7 @@
 namespace Wild\Route;
 function autoload($class){
 	if(strpos($class,__NAMESPACE__.'\\')===0){
-		$file = __DIR__.'/'.substr($class,strlen(__NAMESPACE__)+1).'.php';
+		$file = __DIR__.'/'.str_replace('\\','/',substr($class,strlen(__NAMESPACE__)+1)).'.php';
 		require $file;
 		if(!class_exists($class,false)&&!interface_exists($class,false)&&!trait_exists($class,false))
 			throw new \Exception('Class "'.$class.'" not found as expected in "'.$file.'"');
