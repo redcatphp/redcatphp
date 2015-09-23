5cd6032891f28e43fbf1a2bcc912f3e4f454d914
surikat <jo@surikat.pro>
2015-09-23 18:10:36 +0200

    add _modified auto handling

--- php/Wild/DataMap/EntityModel.php
+++ php/Wild/DataMap/EntityModel.php
@@ -1,6 +1,8 @@
 <?php
 namespace Wild\DataMap;
 class EntityModel implements Observer{
+	protected $__data = [];
+	public $_modified;
 	function beforeRecursive(){}
 	function beforePut(){}
 	function beforeCreate(){}
@@ -13,4 +15,18 @@ class EntityModel implements Observer{
 	function afterUpdate(){}
 	function afterDelete(){}
 	function afterRecursive(){}
+	function __set($k,$v){
+		if(!isset($this->__data[$k])||$this->__data[$k]!==$v)
+			$this->_modified = true;
+		$this->__data[$k] = $v;
+	}
+	function __get($k){
+		return isset($this->__data[$k])?$this->__data[$k]:null;
+	}
+	function __isset($k){
+		return isset($this->__data[$k]);
+	}
+	function __unset($k){
+		unset($this->__data[$k]);
+	}
 }
