2bd804e8e7147e74ad60295e33dae572823689d9
surikat <jo@surikat.pro>
2015-09-30 22:47:49 +0200

    fix up

--- php/Wild/Templix/MarkupX/Pre.php
+++ php/Wild/Templix/MarkupX/Pre.php
@@ -3,6 +3,9 @@ namespace Wild\Templix\MarkupX;
 class Pre extends \Wild\Templix\Markup{
 	protected $noParseContent = true;
 	function load(){
+		if($this->attr('tmp-once'))
+			return;
+		$this->attr('tmp-once',1);
 		$text = $this->getInnerMarkups();
 		$this->clearInner();
 		$this->innerHead[] = htmlentities($text);
