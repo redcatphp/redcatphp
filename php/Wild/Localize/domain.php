bd60fcefa2010e0d5ec8a00c853090606e272128
surikat <jo@surikat.pro>
2015-10-02 02:21:35 +0200

    wip

--- php/Wild/Localize/domain.php
+++ php/Wild/Localize/domain.php
@@ -1,7 +1,7 @@
-<?php namespace Wild\Localize;
-/* Class to hold a single domain included in $text_domains. */
-class domain {
-  var $l10n;
-  var $path;
-  var $codeset;
+<?php
+namespace Wild\Localize;
+class domain{
+	var $l10n;
+	var $path;
+	var $codeset;
 }
