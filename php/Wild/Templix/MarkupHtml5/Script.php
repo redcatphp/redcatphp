e3b733acb8e6d66f1aaae92e2f93f9df98f945e2
surikat <jo@surikat.pro>
2015-10-14 01:54:36 +0200

    isXhtml wrap around script and style

--- php/Wild/Templix/MarkupHtml5/Script.php
+++ php/Wild/Templix/MarkupHtml5/Script.php
@@ -5,5 +5,14 @@ class Script extends \Wild\Templix\Markup{
 	function load(){
 		if(!isset($this->type))
 			$this->type = 'text/javascript';
+		
+		if($this->templix&&$this->templix->isXhtml){
+			$str = trim($this->getInner());
+			if(!empty($str)){
+				if(substr($str,0,13)!='/*<![CDATA[*/'&&substr($str,-7)!='/*]]>*/')
+					$this->innerHead("/*<![CDATA[*/\n");
+					$this->innerFoot("\n/*]]>*/");
+			}
+		}
 	}
 }
--- php/Wild/Templix/MarkupHtml5/Style.php
+++ php/Wild/Templix/MarkupHtml5/Style.php
@@ -2,4 +2,17 @@
 namespace Wild\Templix\MarkupHtml5; 
 class Style extends \Wild\Templix\Markup{
 	protected $noParseContent = true;
+	function load(){
+		if(!isset($this->type))
+			$this->type = 'text/css';
+		
+		if($this->templix&&$this->templix->isXhtml){
+			$str = trim($this->getInner());
+			if(!empty($str)){
+				if(substr($str,0,13)!='/*<![CDATA[*/'&&substr($str,-7)!='/*]]>*/')
+					$this->innerHead("/*<![CDATA[*/\n");
+					$this->innerFoot("\n/*]]>*/");
+			}
+		}
+	}
 }
