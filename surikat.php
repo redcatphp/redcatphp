baed7085c663e917593d082107a72adaf96678a7
surikat <jo@surikat.pro>
2015-10-16 19:13:52 +0200

    add error 500 catch and redirection

--- surikat.php
+++ surikat.php
@@ -22,4 +22,9 @@ else{
 	error_reporting(0);
 	ini_set('display_startup_errors',false);
 	ini_set('display_errors',false);
+	register_shutdown_function(function(){
+		$error = error_get_last();
+		if($error&&$error['type']&(E_ERROR|E_USER_ERROR|E_PARSE|E_CORE_ERROR|E_COMPILE_ERROR|E_RECOVERABLE_ERROR))
+			header('Location: /500',true,302);
+	});
 }
