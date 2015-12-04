df3bb4b47418b31797b5dcfbfa962b50416bf623
surikat <jo@surikat.pro>
2015-12-04 15:44:32 +0100

    add d and dd methods

--- php/RedCat/Debug/functions.inc.php
+++ php/RedCat/Debug/functions.inc.php
@@ -1,13 +1,32 @@
 <?php
-function dbug(){
-	return call_user_func_array(['RedCat\Debug\Vars','debug'],func_get_args());
+if(!function_exists('dbug')){
+	function dbug(){
+		return call_user_func_array(['RedCat\Debug\Vars','debug'],func_get_args());
+	}
 }
-function debug(){
-	return call_user_func_array(['RedCat\Debug\Vars','debug_html'],func_get_args());
+if(!function_exists('debug')){
+	function debug(){
+		return call_user_func_array(['RedCat\Debug\Vars','debug_html'],func_get_args());
+	}
 }
-function dbugs(){
-	return call_user_func_array(['RedCat\Debug\Vars','dbugs'],func_get_args());
+if(!function_exists('dbugs')){
+	function dbugs(){
+		return call_user_func_array(['RedCat\Debug\Vars','dbugs'],func_get_args());
+	}
 }
-function debugs(){
-	return call_user_func_array(['RedCat\Debug\Vars','debugs'],func_get_args());
+if(!function_exists('debugs')){
+	function debugs(){
+		return call_user_func_array(['RedCat\Debug\Vars','debugs'],func_get_args());
+	}
+}
+if(!function_exists('d')){
+	function d(){
+		return call_user_func_array(['RedCat\Debug\Vars',php_sapi_name()=='cli'?'debugsCLI':'debugs'],func_get_args());
+	}
+}
+if(!function_exists('dd')){
+	function dd(){
+		call_user_func_array('d',func_get_args());
+		die;
+	}
 }
