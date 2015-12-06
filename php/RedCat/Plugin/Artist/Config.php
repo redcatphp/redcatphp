9b007ebf372edf2c5babdf16bf8f33c76a88db4c
surikat <jo@surikat.pro>
2015-12-06 12:21:05 +0100

    use tokenized php config manager

--- php/RedCat/Plugin/Artist/Config.php
+++ php/RedCat/Plugin/Artist/Config.php
@@ -2,6 +2,7 @@
 namespace RedCat\Plugin\Artist;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use RedCat\Plugin\PHPConfig\TokenTree;
 class Config extends Artist{
 	protected $description = "Update .config.php file at root of application";
 
@@ -10,90 +11,28 @@ class Config extends Artist{
 		'value'=>"The value to assign",
 	];
 	
-	protected $opts = [
-		'constant'=>"If set, it will set value as constant",
-		'cast'=>"If set, it will cast the value as specified: int, string, float",
-	];
-	
 	protected function execute(InputInterface $input, OutputInterface $output){
 		$key = $input->getArgument('key');
 		$value = $input->getArgument('value');
 		
 		$path = $this->cwd.'.config.php';
-		$config = include($path);
+		$config = new TokenTree($path);
 		
 		if(!$key){
-			$print = self::var_export($config['$']);
+			$print = $config->var_codify($config['$']);
 		}
 		elseif(!isset($value)){
 			$ref = self::dotOffset($key,$config['$']);
-			$print = self::var_export($ref);
+			$print = $config->var_codify($ref);
 		}
 		else{
-
-			if($input->getOption('constant')){
-				$value = constant($value);
-			}
-			elseif($cast=$input->getOption('cast')){
-				switch($cast){
-					case 'const':
-					case 'constant':
-						$value = constant($value);
-					break;
-					case 'boolean':
-					case 'bool':
-						$value = (bool)$value;
-					break;
-					case 'str':
-					case 'string':
-						$value = (string)$value;
-					break;
-					case 'float':
-						$value = (float)$value;
-					break;
-					case 'int':
-					case 'integer':
-						$value = (int)$value;
-					break;
-					case 'a':
-					case 'array':
-						$value = (array)$value;
-					break;
-					case 'obj':
-					case 'object':
-						$value = (object)$value;
-					break;
-				}
-			}
-			else{
-				$value = eval('return '.$value.';');
-			}
 			$ref = self::dotOffset($key,$config['$'],$value);			
-			file_put_contents($path,"<?php\nreturn ".self::var_export($config).';');
-			$print = "$key setted to ".self::var_export($value)." in $path";
+
+			file_put_contents($path,"<?php\nreturn ".(string)$config.';');
+			
+			$print = "$key setted to $value in $path";
 		}
 		$output->writeln($print);
-	}
-	private static function var_export($var, $indent=0){
-		switch(gettype($var)){
-			case 'string':
-				return "'".addcslashes($var, '\'')."'";
-			case 'array':
-				$indexed = array_keys($var) === range(0, count($var) - 1);
-				$r = [];
-				foreach($var as $key => $value){
-					$r[] = str_repeat("\t",$indent+1)
-						 .($indexed?'':self::var_export($key).' => ')
-						 .self::var_export($value, $indent+1);
-				}
-				return "[\n" . implode(",\n", $r) . "\n" . str_repeat("\t",$indent) . "]";
-			case 'boolean':
-				return $var?'true':'false';
-			default:
-				if(is_float($var))
-					return (string)$var;
-				return var_export($var, true);
-		}
 	}
 	private static function dotOffset($dotKey,&$config,$value=null){
 		$dotKey = explode('.',$dotKey);
