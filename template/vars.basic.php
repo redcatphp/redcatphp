28d03a0a39afcd95f42748778e9ad56d9e5ae4ad
surikat <jo@surikat.pro>
2015-12-22 12:02:31 +0100

    update

--- template/vars.basic.php
+++ template/vars.basic.php
@@ -1,4 +1,4 @@
 <?php
 return [
-	'BASE_HREF'		=> RedCat\Wire\Di::getInstance()->create('RedCat\Route\Url')->getBaseHref(),
+	'BASE_HREF'		=> RedCat\Framework\App::getInstance()->create(RedCat\Route\Url::class)->getBaseHref(),
 ];
