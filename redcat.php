5cfaf204c60fc13ea5c21829d355d711bba10a4e
jo <jo@surikat.pro>
2016-11-05 16:15:22 +0100

    packages to vendor

--- redcat.php
+++ redcat.php
@@ -1,6 +1,6 @@
 <?php
 define('REDCAT',__DIR__.'/');
 define('REDCAT_CWD',getcwd().'/');
-$loader = require __DIR__.'/packages/autoload.php';
+$loader = require __DIR__.'/vendor/autoload.php';
 $redcat = RedCat\Framework\App::bootstrap($loader);
 return $redcat;
